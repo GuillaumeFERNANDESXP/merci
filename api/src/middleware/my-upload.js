fe81d67b3dd1b31158b56b20e9dcef52490067b9
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2020-04-18 15:52:31 +0200

    Clean api

--- api/src/middleware/my-upload.js
+++ api/src/middleware/my-upload.js
@@ -17,24 +17,3 @@ module.exports = function (app) {
     res.json(fileRes); // response of this array
   };
 };
-// Load the SDK and UUID
-
-// // Create unique bucket name
-// var bucketName = 'bravoesbucket';
-// // Create name for uploaded object key
-// var keyName = 'heldqdsdlo_world.txt';
-
-// // Create a promise on S3 service object
-
-// // Handle promise fulfilled/rejected states
-// function hello (data) {
-//   // Create params for putObject call
-//   var objectParams = { Bucket: bucketName, Key: keyName, Body: 'Hello World!' };
-//   // Create object upload promise
-//   var uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' }).putObject(objectParams).promise();
-//   uploadPromise.then(
-//     function (data) {
-//       console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-//     });
-// };
-// hello();
