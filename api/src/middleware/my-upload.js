63530ffe4ea94622db21bd5e9e843aa0b4daa031
GuillaumeFERNANDESXP <guillaume.fernandes@hotmail.fr>
2020-04-19 10:11:43 +0200

    Add multer-s3

--- api/src/middleware/my-upload.js
+++ api/src/middleware/my-upload.js
@@ -1,14 +1,9 @@
-const awss3 = require('./awss3');
-
 module.exports = function (app) {
   return async (req, res) => {
-    // console.log(req)
     const { files } = req; // request
     const fileRes = []; // array 
-    // const seq = app.get('mongooseClient');
 
     for (const file of files) try {
-      awss3('hello');
       fileRes.push( // put in this array
         await app.service('drawings').create({
           path: file.path
